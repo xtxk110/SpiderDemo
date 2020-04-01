e1b54dd450978fd4ac9d7ffaa06d1cca2ae30958
xtxk110 <329986833@qq.com>
2020-04-01 15:26:07 +0800

    anglesharp

---
+++ Spider/AngleSharpSpider/AngleSharpHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Html.Parser;
+using AngleSharp.Html.Dom;
+using AngleSharp.Dom;
+
+namespace AngleSharpSpider
+{
+    public class AngleSharpHelper
+    {
+        public async Task<string> GetPageContent(string url)
+        {
+            string result = string.Empty;
+            IBrowsingContext context = BrowsingContext.New(Configuration.Default.WithDefaultLoader().WithJs());
+            IDocument doc=await context.OpenAsync(url);
+            result = doc.DocumentElement.OuterHtml;
+            return result;
+        } 
+    }
+}
---
+++ Spider/AngleSharpSpider/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AngleSharpSpider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url1 = "https://www.360kan.com/dianying/list.php?cat=all&year=all&area=all&act=all&rank=createtime";
+            AngleSharpHelper helper = new AngleSharpHelper();
+            var task = helper.GetPageContent(url1);
+            task.Wait();
+            Console.WriteLine(task.Result);
+
+            Console.Read();
+        }
+    }
+}
